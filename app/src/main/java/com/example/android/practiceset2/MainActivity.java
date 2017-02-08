34b1bcd654b1b79c6c61dede1d5d97e6cc197549
Frau87 <vfrau87@gmail.com>
2017-02-08 12:04:27 +0100

    First changes

--- app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -7,12 +7,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
-
+    int scoreTeamB = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // PASTE CODE YOU WANT TO TEST HERE
+
 
     }
 
@@ -21,24 +21,61 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
 
-    public void threePoints (View view){
+    public void resetScore (View view){
+
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+
+    }
+
+    public void threePointsA (View view){
 
         scoreTeamA += 3;
         displayForTeamA(scoreTeamA);
 
     }
 
-    public void twoPoints (View view){
+    public void threePointsB (View view){
 
-        displayForTeamA(2);
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+
+    }
+
+    public void twoPointsA (View view){
+
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
 
     }
 
-    public void freeThrow (View view){
+    public void twoPointsB (View view){
 
-        displayForTeamA(1);
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
 
     }
+
+    public void freeThrowA (View view){
+
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+
+    }
+
+    public void freeThrowB (View view){
+
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+
+    }
+
 
 }
